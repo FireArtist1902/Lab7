e97081d78f0ca22fcad5df56e47e88160ba913c3
Vadym <vadimvrachenko@gmail.com>
2024-10-22 22:06:25 +0300

    Add from console

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+#include "Windows.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(125);
+    SetConsoleOutputCP(1251);
+
+    cout << "Привіт, Вадим!" <<endl;
+
+    system("pause");
+}
